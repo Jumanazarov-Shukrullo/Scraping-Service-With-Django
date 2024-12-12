182f4bb3b08b3d6953bdd12d3d983bf07726c1b8
Jumanazarov-Shukrullo <shukrullo.jumanazarov@phystech.edu>
2024-12-12 19:04:46 +0300

    update

--- scraping_service_django/settings.py
+++ scraping_service_django/settings.py
@@ -119,3 +119,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+CSRF_TRUSTED_ORIGINS = ['https://scraping-service.up.railway.app/']
